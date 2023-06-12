1a0645f2b3df11f33e3d3527d039c737adedb061
Marco Almeida <marco.almeida@v8tix.com>
2023-06-12 12:46:02 -0500

    added protos

---
+++ generate.go
@@ -0,0 +1,3 @@
+package mallbots_payments_proto
+
+//go:generate buf generate
---
+++ rest/gateway.go
@@ -0,0 +1,28 @@
+package rest
+
+import (
+	"context"
+	"github.com/v8tix/mallbots-payments-proto/pb"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func RegisterGateway(ctx context.Context, mux *chi.Mux, grpcAddr string) error {
+	const apiRoot = "/api/payments"
+
+	gateway := runtime.NewServeMux()
+	err := pb.RegisterPaymentsServiceHandlerFromEndpoint(ctx, gateway, grpcAddr, []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	})
+	if err != nil {
+		return err
+	}
+
+	// mount the GRPC gateway
+	mux.Mount(apiRoot, gateway)
+
+	return nil
+}
